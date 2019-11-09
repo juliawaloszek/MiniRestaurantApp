835e3dc869f0b29c75646186cbebf91c8eac051a
Julia <juliawaloszek@gmail.com>
2019-11-09 18:45:57 +0100

    Add Angular-Slickgrid

--- MiniRestaurantApp-SPA/src/app/app.module.ts
+++ MiniRestaurantApp-SPA/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularSlickgridModule  } from 'angular-slickgrid';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { GridBasicComponent } from './grid-basic/grid-basic.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      TestComponentComponent
+      TestComponentComponent,
+      GridBasicComponent
    ],
    imports: [
+      TranslateModule.forRoot(),
+      AngularSlickgridModule.forRoot(),
       BrowserModule,
       HttpClientModule
    ],
